66faa215be7ac3fc41d8c9e944c7d678be653a25
zalazalaza <elliottrshaw@gmail.com>
2021-05-14 06:25:39 -0400

    rename function for clarity

--- fiki.py
+++ fiki.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def fakeWebsite(urlFiki, string_to_replace, string_to_use):
+def replace_strings(urlFiki, string_to_replace, string_to_use):
     source = requests.get(f'{urlFiki}')
     fiki = BeautifulSoup(source.text, 'lxml')
     mixed_soup = fiki.strings
@@ -14,7 +14,7 @@ def fakeWebsite(urlFiki, string_to_replace, string_to_use):
     return fiki
 
 def main():
-    print(fakeWebsite("ENTER_YOUR_URL_AS_A_STRING_HERE", "some text to find", "replacement text !!!!!!!!!!!fuckyeah!!!!!!!!!!!!!!!!!"))
+    print(replace_strings("ENTER_YOUR_URL_AS_A_STRING_HERE", "some text to find", "replacement text !!!!!!!!!!!fuckyeah!!!!!!!!!!!!!!!!!"))
 
 if __name__=="__main__":
     main()
